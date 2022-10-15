0ba5384861cab1e86450adc36c4d6c151c981445
Shree8622 <93328278+Shree8622@users.noreply.github.com>
2022-10-15 23:52:51 +0530

    Update client.c

--- q7/client.c
+++ q7/client.c
@@ -6,19 +6,15 @@
 #include<string.h>
 #include<sys/time.h>
 char *name;
+
 void display_file()
 {
-    int file = open(name,O_RDONLY);
-    char c[100];
+    char cmd[100];
     printf("Displaying the file contents\n");
-    while(1)
-    {
-        
-        int t = read(file,c,100);
-        printf("%s",c);
-        if(t<100)
-            break;
-    }
+    strcpy(cmd,"cat ");
+    strcat(cmd,name);
+    printf("%s\n",cmd);
+    system(cmd);
     printf("\nExiting...\n");
     exit(0);
 }
@@ -31,51 +27,30 @@ int main(int argc,char* argv[])
 {
     signal(SIGUSR1,display_file);
     signal(SIGUSR2,auth_fail);
+
     int wrt = open("fifo",O_WRONLY);
+
     printf("Enter the file path you want to access\n");
     char path[20];
     scanf("%s",path);
     name=path;
     char *pwd = strdup(getpass("Password: ")); 
-    char str[40];
-    int pid = getpid(),temp=pid,n=1;
-    while(temp>1)
-    {
-        temp/=10;
-        if(temp)
-        n*=10;
-    }
     
-    int i=0;
-    for(;n>0;i++)
-    {
-        str[i]= '0' + pid/n;
-        pid%=n;
-        n/=10;
-    }
-    str[i++]=' ';
-    for(int j=0;j<strlen(path);j++)
-    {
-        str[i+j]=path[j];
-    }
-    i+=strlen(path);
-    str[i++]=' ';
-    char *a = getlogin();
-    for(int j=0;j<strlen(a);j++)
-    {
-        str[i+j]=a[j];
-    }
-    i+=strlen(a);
-    str[i++]=' ';
-    
-    for(int j=0;j<strlen(pwd);j++)
-    {
-        str[i+j]=pwd[j];
-    }
-    // printf("%s\n",str);
-    write(wrt,str,strlen(str));
+    char msg[40];
+    int pid = getpid();
+    char pid_str[10];
+    sprintf(pid_str,"%d",pid);
+    strcat(msg,pid_str);
+    strcat(msg," ");
+
+    char *user = getlogin();
+    strcat(msg,user);
+    strcat(msg," ");
+
+    strcat(msg,pwd);
+
+    write(wrt,msg,strlen(msg));
     while(1)
     {
-
     }
 }
